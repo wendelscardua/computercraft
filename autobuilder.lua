27b6bf60bf6b3d8e1ebe79a838671af6aa812618
Wendel Scardua <wendelscardua@gmail.com>
2021-07-15 00:51:52 -0300

    Prevent issues with full rack

--- autobuilder.lua
+++ autobuilder.lua
@@ -6,19 +6,24 @@ rack = peripheral.wrap("back")
 chest = peripheral.wrap("top")
 
 function processOrder (orderId)
+   local resources = colony.getWorkOrderResources(orderId)
+   if not resources then return end
    print("Processing order "..orderId)
-   for i, resource in pairs(colony.getWorkOrderResources(orderId)) do
+   for i, resource in pairs(resources) do
       if resource.status == "NEED_MORE" or resource.status == "DONT_HAVE" then
          needed = resource.item.count - resource.available - resource.delivering
-         print("Creating "..needed.." of "..resource.item.name)
+         print("Needs "..needed.." of "..resource.item.name)
          while needed > 0 do
             if needed > 64 then
                quantity = 64
             else
                quantity = needed
             end
-            result = commands.replaceItem("block", "~", "~1", "~", "container.0", resource.item.name, quantity)
-            print(textutils.serialize(result))
+            commands.replaceItem("block", "~", "~1", "~", "container.0", resource.item.name, quantity)
+            while rack.size() == #rack.list() do
+               print("Rack is full!")
+               sleep(10)
+            end
             chest.pushItems("back", 1)
             needed = needed - quantity
          end
